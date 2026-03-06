4fd4de8c824179e7451cd0f3993c29b241186ac9
hellomraza <hellomraza@gmail.com>
2026-03-06 20:06:49 +0530

    feat(config): configure mysql typeorm options

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -2,13 +2,12 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const databaseConfig: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT || '3306', 10),
-  username: process.env.DB_USERNAME || 'root',
-  password: process.env.DB_PASSWORD || 'secret',
-  database: process.env.DB_NAME || 'jjm_work_monitoring',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT ?? '3306', 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: process.env.NODE_ENV !== 'production',
   migrations: [__dirname + '/../database/migrations/*{.ts,.js}'],
-  logging: process.env.NODE_ENV === 'development',
 };
