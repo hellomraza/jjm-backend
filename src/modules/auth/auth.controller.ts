408c1425f953e2cca639183f6a99bf21b491fcf1
hellomraza <hellomraza@gmail.com>
2026-03-06 23:59:35 +0530

    feat(auth): add jwt guard, controller and module

---
+++ src/modules/auth/auth.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Post, Body, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { AuthGuard } from '@nestjs/passport';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  @UseGuards(AuthGuard('local'))
+  async login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
+  }
+}
