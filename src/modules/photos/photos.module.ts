0ddac29a267dc19a59170a19ad5b7136c99ab4b9
hellomraza <hellomraza@gmail.com>
2026-03-07 14:35:52 +0530

    feat(photos): add multer upload controller

--- src/modules/photos/photos.module.ts
+++ src/modules/photos/photos.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RolesGuard } from '../../common/guards/roles.guard';
 import { Photo } from './entities/photo.entity';
+import { PhotosController } from './photos.controller';
 import { PhotosService } from './photos.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Photo])],
+  controllers: [PhotosController],
   providers: [PhotosService, RolesGuard],
   exports: [PhotosService],
 })
