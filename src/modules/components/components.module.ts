2f3330bac3785b3a5193ae65ffbf8e65b5e0701e
hellomraza <hellomraza@gmail.com>
2026-03-08 18:52:26 +0530

    feat(components): add component template service

--- src/modules/components/components.module.ts
+++ src/modules/components/components.module.ts
@@ -2,14 +2,16 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RolesGuard } from '../../common/guards/roles.guard';
 import { WorkItem } from '../work-items/entities/work-item.entity';
+import { ComponentTemplateService } from './component-template.service';
 import { ComponentsController } from './components.controller';
 import { ComponentsService } from './components.service';
+import { ComponentTemplate } from './entities/component-template.entity';
 import { Component } from './entities/component.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Component, WorkItem])],
+  imports: [TypeOrmModule.forFeature([Component, WorkItem, ComponentTemplate])],
   controllers: [ComponentsController],
-  providers: [ComponentsService, RolesGuard],
-  exports: [ComponentsService],
+  providers: [ComponentsService, ComponentTemplateService, RolesGuard],
+  exports: [ComponentsService, ComponentTemplateService],
 })
 export class ComponentsModule {}
