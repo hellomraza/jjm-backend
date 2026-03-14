743671b30ed5fe8a7c8407094489b08c671e8cbe
hellomraza <hellomraza@gmail.com>
2026-03-14 23:26:11 +0530

    feat(components): implement photo upload and selection functionality with DTOs

--- src/modules/components/components.module.ts
+++ src/modules/components/components.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RolesGuard } from '../../common/guards/roles.guard';
+import { Photo } from '../photos/entities/photo.entity';
+import { PhotosModule } from '../photos/photos.module';
+import { User } from '../users/entities/user.entity';
 import { WorkItem } from '../work-items/entities/work-item.entity';
 import { ComponentsController } from './components.controller';
 import { ComponentsService } from './components.service';
@@ -8,7 +11,16 @@ import { Component } from './entities/component.entity';
 import { WorkItemComponent } from './entities/work-item-component.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Component, WorkItemComponent, WorkItem])],
+  imports: [
+    TypeOrmModule.forFeature([
+      Component,
+      WorkItemComponent,
+      WorkItem,
+      Photo,
+      User,
+    ]),
+    PhotosModule,
+  ],
   controllers: [ComponentsController],
   providers: [ComponentsService, RolesGuard],
   exports: [ComponentsService],
---
+++ src/modules/components/dto/upload-component-photo.dto.ts
@@ -0,0 +1,32 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsDate, IsLatitude, IsLongitude, IsNotEmpty } from 'class-validator';
+
+export class UploadComponentPhotoDto {
+  @ApiProperty({
+    description: 'Latitude where photo was taken',
+    example: 25.5941,
+  })
+  @IsLatitude()
+  @IsNotEmpty()
+  latitude: number;
+
+  @ApiProperty({
+    description: 'Longitude where photo was taken',
+    example: 85.1376,
+  })
+  @IsLongitude()
+  @IsNotEmpty()
+  longitude: number;
+
+  @ApiProperty({
+    description: 'Photo capture timestamp in ISO format',
+    example: '2026-03-07T10:30:00.000Z',
+    type: String,
+    format: 'date-time',
+  })
+  @Type(() => Date)
+  @IsDate()
+  @IsNotEmpty()
+  timestamp: Date;
+}
