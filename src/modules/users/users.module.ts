aacd09b276792794cb3787d42822f11eb43767cd
hellomraza <hellomraza@gmail.com>
2026-03-24 21:39:19 +0530

    fix: add WorkItemEmployeeAssignment to UsersModule dependencies
    - Import WorkItemEmployeeAssignment entity in users.module.ts
    - Add WorkItemEmployeeAssignment to TypeOrmModule.forFeature
    - Resolves UnknownDependenciesException for UsersService

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RolesGuard } from '../../common/guards/roles.guard';
+import { WorkItemEmployeeAssignment } from '../work-items/entities/work-item-employee-assignment.entity';
 import { User } from './entities/user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, WorkItemEmployeeAssignment])],
   controllers: [UsersController],
   providers: [UsersService, RolesGuard],
   exports: [UsersService],
